6475ee4b53ac1265672c01374341a8829799282d
Andrius <andrius.inside@gmail.com>
2019-04-16 19:58:49 +0300

    Sudoku grid styling added

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -4,9 +4,18 @@ class Cell extends Component {
 
   render() {
     const {value, id, onIncrement, isEmpty} = this.props;
+    let buttonClass = (isEmpty) ? 'btn btn-outline-success cell empty' : 'btn btn-outline-success cell';
+
+    // add thicker vertical lines
+    buttonClass += (id%3 === 0) ? ' border-left' : '';
+    buttonClass += (id%9 === 8) ? ' border-right' : '';
+
+    //add thicker horizontal lines
+    buttonClass += (id < 9 ) ? ' border-top' : '';
+    buttonClass += (id%27 >= 18 ) ? ' border-bottom' : '';
     return (
       <button
-      className={(isEmpty) ? 'btn btn-outline-success cell empty' : 'btn btn-outline-success cell' }
+      className={ buttonClass }
       type="button"
       key={id}
       onClick={() => ((isEmpty) ? onIncrement(this.props.cell) : false)}
